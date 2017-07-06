b76e6d1e1d26ba81b1f3f736f28338be517b6b69
David BOUDAT <david.boudat@ynov.com>
2017-07-06 17:31:41 +0200

    - Mise en place des infoWindows - Début de travail sur les stats de tout les markers visibles

--- FliprPro/Scripts/FliprMap.js
+++ FliprPro/Scripts/FliprMap.js
@@ -8,10 +8,11 @@ function initMap() {
         zoom: 6,
         center: francePos,
         mapTypeId: 'roadmap',
+        streetViewControl: false,
         mapTypeControl: false
     });
 
-    $.get(
+    $.getJSON(
         "https://api.goflipr.com/v1.1/cities",
         function (data) {
             for (var i = 0; i < data.length; i++) {
@@ -25,84 +26,144 @@ function initMap() {
 
                 marker.addListener('click',
                     function () {
-                        $.get(
+                        $.getJSON(
                             "https://api.goflipr.com/v1.1/cities/" + marker.ville.Id + "/pools",
-                            function (data) {
-                                let toto = dataCompliation(data);
+                            function (json) {
+                                let html = makeInfoWindowHtml(dataCompliation(json), marker);
 
                                 var infoWindow = new google.maps.InfoWindow({
-                                    content: "<div><h4>" + marker.ville.Name + " (" + marker.ville.ZipCode + ")<h4><br/>"
-
+                                    content: html
                                 });
 
                                 if (activeInfoWindow != undefined)
                                     activeInfoWindow.close();
                                 infoWindow.open(map, marker);
                                 activeInfoWindow = infoWindow;
-                            },
-                            "json"
+                            }
                         );
                     });
 
                 markers.push(marker);
             }
 
+            google.maps.event.addListener(map, 'bounds_changed', function () {
+                bounds = map.getBounds();
+                let array = [];
+                for (let i = 0; i < markers.length; i++) {
+                    if (map.getBounds().contains(markers[i].getPosition())) {
+                    }
+                }
+            });
+
             var markerCluster = new MarkerClusterer(map, markers,
                 { imagePath: "/Images/markerclusterer/m" });
-        },
-        "json"
+        }
     );
 }
 
-function dataCompliation(data) {
-
-    let toto = new Object(); //TODO : trouver un meilleur nom
-
-    let AnConstMoy = 0;
-
-    toto.Coating = new Map();
-    toto.Integration = new Map();
-    toto.Shape = new Map();
-    toto.Filtration = new Map();
-    toto.Treatment = new Map();
 
 
-    for (i = 0; i < data.length; i++) {
-        AnConstMoy = (AnConstMoy + data[i].BuiltYear) / 2;
+function dataCompliation(json) {
+    let data = new Object();
 
-        let coatingNb = toto.Coating.get(data[i].Coating);
+    let ageSum = 0;
+    data.Coating = new Map();
+    data.Integration = new Map();
+    data.Shape = new Map();
+    data.Filtration = new Map();
+    data.Treatment = new Map();
+    data.NbSwPool = json.length;
+
+    let test = json[0].Coating === json[0].Coating;
+
+    for (i = 0; i < json.length; i++) {
+        ageSum += new Date().getFullYear() - json[i].BuiltYear;
+
+        let coatingNb = data.Coating.get(json[i].Coating.Name);
         if (coatingNb == undefined)
-            toto.Coating.set(data[i].Coating, 1);
+            data.Coating.set(json[i].Coating.Name, 1);
         else
-            toto.Coating.set(data[i].Coating, coatingNb + 1);
+            data.Coating.set(json[i].Coating.Name, coatingNb + 1);
 
-        let IntegrationNb = toto.Integration.get(data[i].Integration); 
+        let IntegrationNb = data.Integration.get(json[i].Integration.Name);
         if (IntegrationNb == undefined)
-            toto.Integration.set(data[i].Integration, 1);
+            data.Integration.set(json[i].Integration.Name, 1);
         else
-            toto.Integration.set(data[i].Integration, IntegrationNb + 1);
+            data.Integration.set(json[i].Integration.Name, IntegrationNb + 1);
 
-        let ShapeNb = toto.Shape.get(data[i].Shape); 
+        let ShapeNb = data.Shape.get(json[i].Shape.Name);
         if (ShapeNb == undefined)
-            toto.Shape.set(data[i].Shape, 1);
+            data.Shape.set(json[i].Shape.Name, 1);
         else
-            toto.Shape.set(data[i].Shape, ShapeNb + 1);
+            data.Shape.set(json[i].Shape.Name, ShapeNb + 1);
 
-        let FiltrationNb = toto.Filtration.get(data[i].Filtration); 
+        let FiltrationNb = data.Filtration.get(json[i].Filtration.Name);
         if (FiltrationNb == undefined)
-            toto.Filtration.set(data[i].Filtration, 1);
+            data.Filtration.set(json[i].Filtration.Name, 1);
         else
-            toto.Filtration.set(data[i].Filtration, FiltrationNb + 1);
+            data.Filtration.set(json[i].Filtration.Name, FiltrationNb + 1);
 
-        let TreatmentNb = toto.Treatment.get(data[i].Treatment); 
+        let TreatmentNb = data.Treatment.get(json[i].Treatment.Name);
         if (TreatmentNb == undefined)
-            toto.Treatment.set(data[i].Treatment, 1);
+            data.Treatment.set(json[i].Treatment.Name, 1);
         else
-            toto.Treatment.set(data[i].Treatment, TreatmentNb + 1);
+            data.Treatment.set(json[i].Treatment.Name, TreatmentNb + 1);
 
     }
 
-    toto.age = new Date().getFullYear - AnConstMoy;
+    data.age = Math.round(ageSum / json.length);
 
-    return toto;
+    return data;
+}
+
+function makeInfoWindowHtml(data, marker) {
+    let html = "<h4>" + marker.ville.Name + " (" + marker.ville.ZipCode + ")</h4><br/>";
+
+    html += "<p>Moyenne d'age des piscines : " + data.age + " ans</p>";
+    html += "<p>Nombre de piscine : " + data.NbSwPool + "</p>";
+
+    html += "<div class='panel-group' id='accordion' role='tablist' aria-multiselectable='true'>";
+
+    //Integration
+    html += "<div class='panel panel-primary'><div class='panel-heading' role='tab' id='Heading-IntegrPan'><h4 class='panel-title'><a role='button' data-toggle='collapse' data-parent='#accordion' href='#Collapse-IntegrPan' aria-expanded='true' aria-controls='Collapse-IntegrPan'>Type de piscines</a></h4></div><div id='Collapse-IntegrPan' class='panel-collapse collapse in' role='tabpanel' aria-labelledby='Heading-IntegrPan'>";
+    html += "<table table-bordered class='table table-bordered table table-bordered-bordered'><tr><th>Type de piscine</th><th>Nombre</th></tr>";
+    for (let [type, number] of data.Integration) {
+        html += "<tr><td>" + type + "</td><td>" + number + "</td></tr>";
+    }
+    html += "</table></div></div>";
+
+    //Shape
+    html += "<div class='panel panel-primary'><div class='panel-heading' role='tab' id='Heading-ShapePan'><h4 class='panel-title'><a role='button' data-toggle='collapse' data-parent='#accordion' href='#Collapse-ShapePan' aria-expanded='true' aria-controls='Collapse-ShapePan'>Formes</a></h4></div><div id='Collapse-ShapePan' class='panel-collapse collapse' role='tabpanel' aria-labelledby='Heading-ShapePan'>";
+    html += "<table table-bordered class='table table-bordered'><tr><th>Forme des piscines</th><th>Nombre</th></tr>";
+    for (let [type, number] of data.Shape) {
+        html += "<tr><td>" + type + "</td><td>" + number + "</td></tr>";
+    }
+    html += "</table></div></div>";
+
+    //Coating
+    html += "<div class='panel panel-primary'><div class='panel-heading' role='tab' id='Heading-CoatingPan'><h4 class='panel-title'><a role='button' data-toggle='collapse' data-parent='#accordion' href='#Collapse-CoatingPan' aria-expanded='true' aria-controls='Collapse-CoatingPan'>Surfaces</a></h4></div><div id='Collapse-CoatingPan' class='panel-collapse collapse' role='tabpanel' aria-labelledby='Heading-CoatingPan'>";
+    html += "</table><table table-bordered class='table table-bordered'><tr><th>Type de surface</th><th>Nombre</th></tr>";
+    for (let [type, number] of data.Coating) {
+        html += "<tr><td>" + type + "</td><td>" + number + "</td></tr>";
+    }
+    html += "</table></div></div>";
+
+    //Filtration
+    html += "<div class='panel panel-primary'><div class='panel-heading' role='tab' id='Heading-FiltrationPan'><h4 class='panel-title'><a role='button' data-toggle='collapse' data-parent='#accordion' href='#Collapse-FiltrationPan' aria-expanded='true' aria-controls='Collapse-FiltrationPan'>Filtrations</a></h4></div><div id='Collapse-FiltrationPan' class='panel-collapse collapse' role='tabpanel' aria-labelledby='Heading-FiltrationPan'>";
+    html += "</table><table table-bordered class='table table-bordered'><tr><th>Type de filtration</th><th>Nombre</th></tr>"
+    for (let [type, number] of data.Filtration) {
+        html += "<tr><td>" + type + "</td><td>" + number + "</td></tr>";
+    }
+    html += "</table></div></div>";
+
+    //Treatment
+    html += "<div class='panel panel-primary'><div class='panel-heading' role='tab' id='Heading-TreatmentPan'><h4 class='panel-title'><a role='button' data-toggle='collapse' data-parent='#accordion' href='#Collapse-TreatmentPan' aria-expanded='true' aria-controls='Collapse-TreatmentPan'>Traitements</a></h4></div><div id='Collapse-TreatmentPan' class='panel-collapse collapse' role='tabpanel' aria-labelledby='Heading-TreatmentPan'>";
+    html += "</table><table table-bordered class='table table-bordered'><tr><th>Type de traitement</th><th>Nombre</th></tr>";
+    for (let [type, number] of data.Treatment) {
+        html += "<tr><td>" + type + "</td><td>" + number + "</td></tr>";
+    }
+
+    html += "</table></div></div></div>";
+
+    return html;
 }
